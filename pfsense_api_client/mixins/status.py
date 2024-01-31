40f5d23ac840f07d04f547248d0b60bfb2eafa3a
Devin Barry <devinbarry@users.noreply.github.com>
2024-01-31 17:59:18 +0000

    Improve readability.

--- pfsense_api_client/mixins/status.py
+++ pfsense_api_client/mixins/status.py
@@ -10,18 +10,14 @@ from pfsense_api_client.api_types import APIResponse, APIResponseDict
 
 class StatusMixin(ClientBase):
     """ status calls """
-    def get_carp_status(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_carp_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-carp-status"""
         url = "/api/v1/status/carp"
         return self.call(url, payload=filterargs)
 
 
     @pydantic.validate_arguments()
-    def update_carp_status(
-        self, enable: Optional[bool], maintenance_mode: Optional[bool]
-    ) -> requests.Response:
+    def update_carp_status(self, enable: Optional[bool], maintenance_mode: Optional[bool]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-carp-status"""
         url = "/api/v1/status/carp"
         method = "PUT"
@@ -33,66 +29,49 @@ class StatusMixin(ClientBase):
         return self.call(url=url, method=method, payload=payload)
 
 
-    def get_gateway_status(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_gateway_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-gateway-status"""
         url = "/api/v1/status/gateway"
         return self.call(url, payload=filterargs)
 
 
-    def get_interface_status(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_interface_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-interface-status"""
         url = "/api/v1/status/interface"
         return self.call(url, payload=filterargs)
 
 
-    def get_configuration_history_status_log(
-        self,
-        **filterargs: Dict[str, Any],
-    ) -> requests.Response:
+    def get_configuration_history_status_log(self, **filterargs: Dict[str, Any],) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-configuration-history-status-log"""
         url = "/api/v1/status/log/config_history"
         return self.call(url, payload=filterargs)
 
 
-    def get_dhcp_status_log(
-        self, **filterargs: Dict[str, Any]
-    ) -> APIResponse:
+    def get_dhcp_status_log(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-read-dhcp-status-log"""
         url = "/api/v1/status/log/dhcp"
         return self.call_api(url, payload=filterargs)
 
 
-    def get_firewall_status_log(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_firewall_status_log(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-firewall-status-log"""
         url = "/api/v1/status/log/firewall"
         return self.call(url, payload=filterargs)
 
 
-    def get_system_status_log(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_system_status_log(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#4-read-system-status-log"""
         url = "/api/v1/status/log/system"
         return self.call(url, payload=filterargs)
 
 
-    def get_openvpn_status(
-        self, **filterargs: Dict[str, Any]
-    ) -> requests.Response:
+    def get_openvpn_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-openvpn-status"""
         url = "/api/v1/status/openvpn"
         return self.call(url, payload=filterargs)
 
 
-    def get_system_status(
-        self, **filterargs: Dict[str, Any]
-    ) -> APIResponseDict:
+    def get_system_status(self, **filterargs: Dict[str, Any]) -> APIResponseDict:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-status"""
         url = "/api/v1/status/system"
         result = APIResponseDict.parse_obj(self.call_json(url, payload=filterargs))
