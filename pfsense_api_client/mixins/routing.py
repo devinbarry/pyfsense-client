3b361ceee695ac006648f4a83f330e4d10abbcf8
Devin Barry <devinbarry@users.noreply.github.com>
2024-02-01 18:03:03 +0000

    Explicit is better than implicit.

--- pfsense_api_client/mixins/routing.py
+++ pfsense_api_client/mixins/routing.py
@@ -29,7 +29,7 @@ class RoutingMixin(ClientABC):
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-routing-gateways"""
 
         url = "/api/v1/routing/gateway"
-        return self.call(url=url, payload=filterargs)
+        return self.call(url=url, method='GET', payload=filterargs)
 
 
     def update_routing_gateway(self, **args: Dict[str, Any]) -> APIResponse:
@@ -43,7 +43,7 @@ class RoutingMixin(ClientABC):
     def get_routing_gateway_details(self, **filterargs: Dict[str, Any],) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-routing-gateway-details"""
         url = "/api/v1/routing/gateway/detail"
-        return self.call(url=url, payload=filterargs)
+        return self.call(url=url, method='GET', payload=filterargs)
 
 
     def create_static_route(self, **args: Dict[str, Any]) -> APIResponse:
@@ -63,7 +63,7 @@ class RoutingMixin(ClientABC):
     def get_static_route(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-static-routes"""
         url = "/api/v1/routing/static_route"
-        return self.call(url=url, payload=filterargs)
+        return self.call(url=url, method='GET', payload=filterargs)
 
 
     def update_static_route(self, **args: Dict[str, Any]) -> APIResponse:
