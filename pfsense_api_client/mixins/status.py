c01ec6ab57788c3cc99f8d21d7fac18cde825e77
Devin Barry <devinbarry@users.noreply.github.com>
2024-02-01 17:30:20 +0000

    Everything now returns an APIResponse.

--- pfsense_api_client/mixins/status.py
+++ pfsense_api_client/mixins/status.py
@@ -1,23 +1,20 @@
 """ status-related endpoints """
 from typing import Any, Dict, Optional
-
-import requests
 from pydantic import validate_call
 
-from ..client import ClientABC
-from ..client.types import APIResponse
+from ..client import ClientABC, APIResponse
 
 
 class StatusMixin(ClientABC):
     """ status calls """
-    def get_carp_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_carp_status(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-carp-status"""
         url = "/api/v1/status/carp"
         return self.call(url=url, payload=filterargs)
 
 
     @validate_call
-    def update_carp_status(self, enable: Optional[bool], maintenance_mode: Optional[bool]) -> requests.Response:
+    def update_carp_status(self, enable: Optional[bool], maintenance_mode: Optional[bool]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-carp-status"""
         url = "/api/v1/status/carp"
         method = "PUT"
@@ -29,19 +26,19 @@ class StatusMixin(ClientABC):
         return self.call(url=url, method=method, payload=payload)
 
 
-    def get_gateway_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_gateway_status(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-gateway-status"""
         url = "/api/v1/status/gateway"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_interface_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_interface_status(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-interface-status"""
         url = "/api/v1/status/interface"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_configuration_history_status_log(self, **filterargs: Dict[str, Any],) -> requests.Response:
+    def get_configuration_history_status_log(self, **filterargs: Dict[str, Any],) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-configuration-history-status-log"""
         url = "/api/v1/status/log/config_history"
         return self.call(url=url, payload=filterargs)
@@ -53,19 +50,19 @@ class StatusMixin(ClientABC):
         return self.call(url, payload=filterargs)
 
 
-    def get_firewall_status_log(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_firewall_status_log(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-firewall-status-log"""
         url = "/api/v1/status/log/firewall"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_system_status_log(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_status_log(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#4-read-system-status-log"""
         url = "/api/v1/status/log/system"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_openvpn_status(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_openvpn_status(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-openvpn-status"""
         url = "/api/v1/status/openvpn"
         return self.call(url=url, payload=filterargs)
--- pfsense_api_client/mixins/system.py
+++ pfsense_api_client/mixins/system.py
@@ -1,12 +1,9 @@
 """ system-related endpoints """
 
 from typing import Any, Dict, List, Optional
-
 import pydantic
-import requests
 
-from ..client import ClientABC
-from ..client.types import APIResponse
+from ..client import ClientABC, APIResponse
 
 
 class SystemMixin(ClientABC):
@@ -31,7 +28,7 @@ class SystemMixin(ClientABC):
 
 
     def update_system_api_configuration(self, readonly: Optional[bool] = None,
-                                        **kwargs: Dict[str, Any]) -> requests.Response:
+                                        **kwargs: Dict[str, Any]) -> APIResponse:
         """Update the API configuration.
 
         https://github.com/jaredhendrickson13/pfsense-api#3-read-system-api-version
@@ -62,192 +59,192 @@ class SystemMixin(ClientABC):
         return self.call(url=url, method=method, payload=payload.dict())
 
 
-    def get_system_api_configuration(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_api_configuration(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-api-configuration"""
         url = "/api/v1/system/api"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_system_api_error_library(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_api_error_library(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-read-system-api-error-library"""
         url = "/api/v1/system/api/error"
         return self.call(url=url, payload=filterargs)
 
 
-    def delete_system_arp_table(self, **args: Dict[str, Any]) -> requests.Response:
+    def delete_system_arp_table(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-delete-system-arp-table"""
         url = "/api/v1/system/arp"
         method = "DELETE"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_arp_table(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_arp_table(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-read-system-arp-table"""
         url = "/api/v1/system/arp"
         return self.call(url=url, payload=filterargs)
 
 
-    def create_system_ca(self, **args: Dict[str, Any]) -> requests.Response:
+    def create_system_ca(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-create-system-ca"""
         url = "/api/v1/system/ca"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def delete_system_ca(self, **args: Dict[str, Any]) -> requests.Response:
+    def delete_system_ca(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-delete-system-ca"""
         url = "/api/v1/system/ca"
         method = "DELETE"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_cas(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_cas(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-system-cas"""
         url = "/api/v1/system/ca"
         return self.call(url=url, payload=filterargs)
 
 
-    def create_system_certificates(self, **args: Dict[str, Any]) -> requests.Response:
+    def create_system_certificates(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-create-system-certificates"""
         url = "/api/v1/system/certificate"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def delete_system_certificates(self, **args: Dict[str, Any]) -> requests.Response:
+    def delete_system_certificates(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-delete-system-certificates"""
         url = "/api/v1/system/certificate"
         method = "DELETE"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_certificates(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_certificates(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-system-certificates"""
         url = "/api/v1/system/certificate"
         return self.call(url=url, payload=filterargs)
 
 
-    def get_system_configuration(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_configuration(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-configuration"""
         url = "/api/v1/system/config"
         return self.call(url=url, payload=filterargs)
 
 
-    def update_system_configuration(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_system_configuration(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-system-configuration"""
         url = "/api/v1/system/config"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def update_console_settings(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_console_settings(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-update-console-settings"""
         url = "/api/v1/system/console"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_dns(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_dns(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-dns"""
         url = "/api/v1/system/dns"
         return self.call(url=url, payload=filterargs)
 
 
-    def update_system_dns(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_system_dns(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-system-dns"""
         url = "/api/v1/system/dns"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def create_system_dns_server(self, **args: Dict[str, Any]) -> requests.Response:
+    def create_system_dns_server(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-create-system-dns-server"""
         url = "/api/v1/system/dns/server"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def delete_system_dns_server(self, **args: Dict[str, Any]) -> requests.Response:
+    def delete_system_dns_server(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-delete-system-dns-server"""
         url = "/api/v1/system/dns/server"
         method = "DELETE"
         return self.call(url=url, method=method, payload=args)
 
 
-    def halt_system(self, **args: Dict[str, Any]) -> requests.Response:
+    def halt_system(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-halt-system"""
         url = "/api/v1/system/halt"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_hostname(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_hostname(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-hostname"""
         url = "/api/v1/system/hostname"
         return self.call(url=url, payload=filterargs)
 
 
-    def update_system_hostname(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_system_hostname(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-system-hostname"""
         url = "/api/v1/system/hostname"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_email_notification_settings(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_email_notification_settings(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-email-notification-settings"""
         url = "/api/v1/system/notifications/email"
         return self.call(url=url, payload=filterargs)
 
 
-    def update_system_email_notification_settings(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_system_email_notification_settings(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-update-system-email-notification-settings"""
         url = "/api/v1/system/notifications/email"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def create_system_reboot(self, **args: Dict[str, Any]) -> requests.Response:
+    def create_system_reboot(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-create-system-reboot"""
         url = "/api/v1/system/reboot"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_tables(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_tables(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-tables"""
         url = "/api/v1/system/table"
         return self.call(url=url, payload=filterargs)
 
 
-    def create_system_tunables(self, **args: Dict[str, Any]) -> requests.Response:
+    def create_system_tunables(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-create-system-tunables"""
         url = "/api/v1/system/tunable"
         method = "POST"
         return self.call(url=url, method=method, payload=args)
 
 
-    def delete_system_tunables(self, **args: Dict[str, Any]) -> requests.Response:
+    def delete_system_tunables(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#2-delete-system-tunables"""
         url = "/api/v1/system/tunable"
         method = "DELETE"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_tunables(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_tunables(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#3-read-system-tunables"""
         url = "/api/v1/system/tunable"
         return self.call(url=url, payload=filterargs)
 
 
-    def update_system_tunables(self, **args: Dict[str, Any]) -> requests.Response:
+    def update_system_tunables(self, **args: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#4-update-system-tunables"""
         url = "/api/v1/system/tunable"
         method = "PUT"
         return self.call(url=url, method=method, payload=args)
 
 
-    def get_system_version(self, **filterargs: Dict[str, Any]) -> requests.Response:
+    def get_system_version(self, **filterargs: Dict[str, Any]) -> APIResponse:
         """https://github.com/jaredhendrickson13/pfsense-api/blob/master/README.md#1-read-system-version"""
         url = "/api/v1/system/version"
         return self.call(url=url, payload=filterargs)
