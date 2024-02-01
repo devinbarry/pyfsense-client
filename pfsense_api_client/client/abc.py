e2a98c7a8b514fc67b3a3e98296063d7055fcdc2
Devin Barry <devinbarry@users.noreply.github.com>
2024-02-01 17:37:14 +0000

    Rename private method.

--- pfsense_api_client/client/abc.py
+++ pfsense_api_client/client/abc.py
@@ -7,7 +7,7 @@ from .types import APIResponse
 class ClientABC(ABC):
 
     @abstractmethod
-    def _call(self, url, method="GET", payload=None, params=None, **kwargs) -> Response:
+    def _request(self, url, method="GET", payload=None, params=None, **kwargs) -> Response:
         pass
 
     @abstractmethod
